21b0bbf2e450a77bd0c2d9cb4f0abccf3f8fc6fe
TochuGV <tobiasvallejos05@gmail.com>
2026-02-25 20:27:06 -0300

    chore(hooks): change file extensions from .tsx to .ts

--- src/hooks/useCarousel.ts
+++ src/hooks/useCarousel.ts
@@ -1,26 +1,27 @@
 import { useState } from "react"
 
 const useCarousel = (length: number) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   const nextSlide = () => {
-    setCurrentIndex((prev) => (prev === length - 1 ? 0 : prev + 1));
-  };
+    setCurrentIndex((prev) => (prev === length - 1 ? 0 : prev + 1))
+  }
 
   const prevSlide = () => {
-    setCurrentIndex((prev) => (prev === 0 ? length - 1 : prev - 1));
-  };
+    setCurrentIndex((prev) => (prev === 0 ? length - 1 : prev - 1))
+  }
 
   const goToSlide = (index: number) => {
-    setCurrentIndex(index);
-  };
+    setCurrentIndex(index)
+  }
 
   return {
     currentIndex,
     nextSlide,
     prevSlide,
     goToSlide,
-  };
-};
+    setCurrentIndex
+  }
+}
 
 export default useCarousel
